4ea2f3aee5720c96e2d98b95416762bf0dbde0bd
Cadris <cadrissilver@gmail.com>
2022-02-03 11:08:09 +0530

    Innitial Commit

---
+++ src/main/java/com/sayan/brainfuck/BrainfuckApplication.java
@@ -0,0 +1,13 @@
+package com.sayan.brainfuck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainfuckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainfuckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayan/brainfuck/BrainfuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayan.brainfuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainfuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
